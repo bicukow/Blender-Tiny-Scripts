3f4323f733a93d472b84d3f818d1864030c9034b
bicukow <94756453+bicukow@users.noreply.github.com>
2025-01-21 16:30:28 +0300

    Update Node Groups - Merge base names.py

--- Node Groups - Merge base names.py
+++ Node Groups - Merge base names.py
@@ -1,27 +1,26 @@
 import bpy
 from re import search
 
-def delete_single_tails():              #delete ".001" tail if node is single
-    for i in bpy.data.node_groups:
-        if search('.\d\d\d', i.name):
-            if bpy.data.node_groups.find(i.name[:-4]):
-                print('will be renamed:', i.name)
-                i.name = i.name[:-4]
-                
-delete_single_tails()
+#remove ".001" tail if node is single
+for i in bpy.data.node_groups:
+    if search('.\d\d\d', i.name):
+        if bpy.data.node_groups.find(i.name[:-4]):
+            print('will be renamed:', i.name)
+            i.name = i.name[:-4]
        
 def replace_node_groups():
-    if node.type == 'GROUP' and search('.\d\d\d', node.node_tree.name):
-        node.node_tree = bpy.data.node_groups[node.node_tree.name[:-4]]
+    if node.type == 'GROUP' and search('.\d\d\d', node.node_tree.name): # search for group node AND .001 tails
+        if 'Geometry Nodes' not in node.node_tree.name: # do not search for node groups with default names
+            if 'NodeGroup' not in node.node_tree.name:  # do not search for node groups with default names
+                node.node_tree = bpy.data.node_groups[node.node_tree.name[:-4]]
 
-for node_group in bpy.data.node_groups: # for node_groups (geometry nodes)
+## for node_groups (geometry nodes)
+for node_group in bpy.data.node_groups:
     for node in node_group.nodes:
         replace_node_groups()
 
-for mats in bpy.data.materials:         # for materials
+## for materials
+for mats in bpy.data.materials: 
     for node in mats.node_tree.nodes:
-        replace_node_groups()
-            
-
-        
+        replace_node_groups()     
         
